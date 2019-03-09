6c71a69cdb5f9fb3b15f73e6d32fdb7426c1063a
HyeongTak <andy395023@gmail.com>
2019-03-09 22:33:03 +0900

    네비게이션, 정보 수정

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(express.static('public'));
+
+app.set('views', __dirname + '/views');
+
+
+var port =  process.env.PORT || 3000;
